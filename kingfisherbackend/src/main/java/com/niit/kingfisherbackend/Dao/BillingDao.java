778c5167ccdf6f76fc9f3720adb374432b18f3be
Ramya H.R <ramya.rajbha22@gmail.com>
2018-03-31 19:15:20 +0530

    Completed project
    Project completed

---
+++ kingfisherbackend/src/main/java/com/niit/kingfisherbackend/Dao/BillingDao.java
@@ -0,0 +1,12 @@
+package com.niit.kingfisherbackend.Dao;
+
+import java.util.List;
+
+import com.niit.kingfisherbackend.model.Billing;
+
+public interface BillingDao {
+	public boolean Saveorupdate(Billing billing);
+	public boolean delete(String bill_id);
+	public Billing get(String u_Id);
+	public List <Billing> list();
+}
